9f78a0c5f30f7fdd1bfba46db9e5ae290a206292
chand999-coder <chandhinibalasubramaniamtn@gmail.com>
2025-09-27 16:48:32 -0700

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,7 @@ $config['version'] = "1.0.0";
 
 //Cache Settings
 define("cache_status", 0); //defaults to 1 that is enabled. 
-define('cache_path', dirname(__FILE__).'/temp/'); //replace it with your path.	Dont change the directory if deploying on heroku and probably wont work on heroku.
+//define('cache_path', dirname(__FILE__).'/temp/'); //replace it with your path.	Dont change the directory if deploying on heroku and probably wont work on heroku.
+define('cache_path', '/tmp/');
 define('cache_meta_ttl',86400); //meta of stream cache time	in s; defaults to 1 day
 define('cache_catalog_ttl',3600); //catalog of tag cache time in s; defaults to 1 hour
